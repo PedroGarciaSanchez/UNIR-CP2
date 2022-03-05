01687f6ce1fbe5f677236f6d838f7c9542bc06c6
B1420329 <SmartP00>
2022-03-05 09:00:07 +0100

    nfs install 6

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-ansible-playbook -i hosts.azuremio 01-playbook.yaml
-ansible-playbook -i hosts.azuremio -l master 02-playbook.yaml
+ssh-keygen -f known_hosts x.x.x.master
+ssh-keygen -f known_hosts x.x.x.worker1
+ssh-keygen -f known_hosts x.x.x.nfs
+
+ansible-playbook -i hosts k8s-01-common.yaml
+ansible-playbook -i hosts -l nfs nfs.yaml
 #con este deploy no va a desplegar. 
 #cuando ejecute el pbook que inicializa el cluster(que tengo que poner aquí), me va a decir que 
 #haga a mano lo de ejecutar el JOIN (ver "Tareas para desplegar K8s.doc"). Redirigir la salida a un 
