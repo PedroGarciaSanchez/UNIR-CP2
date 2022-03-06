1d228ea31207f3744c9a9aed55dc66ead28dcf3d
B1420329 <SmartP00>
2022-03-06 09:45:52 +0100

    fix kubelet problem

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #ssh-keygen -f C:\Users\usuariox\.ssh\known_hosts -R 10.71.149.89
-ansible-galaxy collection install ansible.posix
+sudo ansible-galaxy collection install ansible.posix
 #Antes de lanzar el primer pllaybook, que va a atodas las máquinas
-sudo ssh-keygen -f /home/pedrogsa/known_hosts -R 20.123.180.50
-sudo ssh-keygen -f /home/pedrogsa/known_hosts -R 20.23.228.230
-sudo ssh-keygen -f /home/pedrogsa/known_hosts -R 20.123.182.147
+#entrar en las máquinas y salir con logout, para que quedeen registradas en known_hosts
+sudo ssh adminUsername@20.123.180.50
+sudo ssh adminUsername@20.23.228.230
+sudo ssh adminUsername@20.123.182.147
 
-ansible-playbook -i hosts k8s-01-common.yaml
-ansible-playbook -i hosts -l nfs nfs.yaml
-ansible-playbook -i hosts -l master,worker k8s-05-master-worker.yaml
+sudo ansible-playbook -i hosts k8s-01-common.yaml
+sudo ansible-playbook -i hosts -l nfs nfs.yaml
+sudo ansible-playbook -i hosts -l master,worker k8s-05-master-worker.yaml
 #ansible-playbook -i hosts -l worker k8s-05-master-worker.yaml
 #con este deploy no va a desplegar. 
 #cuando ejecute el pbook que inicializa el cluster(que tengo que poner aquí), me va a decir que 
