b624da5e5009e76078897f98225c7a5c5f4b6311
B1420329 <SmartP00>
2022-03-06 08:26:39 +0100

    master-worker 13

--- ansible/deploy_after_manual_task.sh
+++ ansible/deploy_after_manual_task.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
-
-ansible-playbook -i hosts -l master xxxxx.yaml
+ansible-playbook -i hosts -l master k8s-02-master_after_manual.yaml
+ansible-playbook -i hosts -l worker k8s-03-workers.yaml
