d0c3c9fa82ec0019bb1005a0f7b1b7be4e6e947b
B1420329 <SmartP00>
2022-03-07 10:07:36 +0100

    1 more cpu for master

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,23 +7,23 @@ variable "location" {
 variable "vm_size_master" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 20 GB, 2 CPU 
+  default = "Standard_D2_v2" # 20 GB, 2 CPU 
 }
 
 variable "vm_size_worker1" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 20 GB, 1 CPU 
+  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
 }
 
 variable "vm_size_worker2" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 20 GB, 1 CPU 
+  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
 }
 
 variable "vm_size_nfs" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 20 GB, 1 CPU 
+  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
 }
