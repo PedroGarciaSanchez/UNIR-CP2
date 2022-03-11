76b101b7c48d434c834f1761db4bf870c9f8efdb
B1420329 <SmartP00>
2022-03-11 10:03:43 +0100

    despliegue jenkins 1

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #ssh-keygen -f C:\Users\usuariox\.ssh\known_hosts -R 10.71.149.89
-sudo ansible-galaxy collection install ansible.posix
+#sudo ansible-galaxy collection install ansible.posix
 #Antes de lanzar el primer pllaybook, que va a atodas las máquinas
 #entrar en las máquinas y salir con logout, para que quedeen registradas en known_hosts
-sudo ssh adminUsername@40.68.119.5
-sudo ssh adminUsername@20.107.7.92
-sudo ssh adminUsername@13.95.17.175
+#sudo ssh adminUsername@40.68.119.5
+#sudo ssh adminUsername@20.107.7.92
+#sudo ssh adminUsername@13.95.17.175
 
 
 
@@ -14,16 +14,21 @@ sudo ansible-playbook -i hosts k8s-01-common.yaml
 sudo ansible-playbook -i hosts -l nfs nfs.yaml
 sudo ansible-playbook -i hosts -l master,worker k8s-05-master-worker.yaml
 sudo ansible-playbook -i hosts -l master k8s-02-master.yaml
-#parte manual
-sudo ansible-playbook -i hosts -l master k8s-02-master_after_manual.yaml
-sudo ansible-playbook -i hosts -l worker,master k8s-02-worker_after_manual.yaml
-sudo ansible-playbook -i hosts -l master k8s-02-master_after_reboot.yaml
+#parte manual en master:
+# sudo kubeadm init --pod-network-cidr 192.169.1.0/16
 
-sudo ansible-playbook -i hosts -l worker k8s-03-workers.yaml
 
-#ansible-playbook -i hosts -l worker k8s-05-master-worker.yaml
-#con este deploy no va a desplegar. 
-#cuando ejecute el pbook que inicializa el cluster(que tengo que poner aquí), me va a decir que 
-#haga a mano lo de ejecutar el JOIN (ver "Tareas para desplegar K8s.doc"). Redirigir la salida a un 
-#fichero, enla maq lo copiamos a mano, lo metemos en variable de playbook y 
-# lanzamos deploy.sh (pag 131)
+#output: 
+# las instrucciones que salen las ejecuto después en k8s-03-workers.yaml
+# Guardo el comando kubeadm que sale al final, ya que lo necesitaré para unir los workers al clúster.
+#Cambio en group_vars/k8s-worker.yaml los valores:
+#-token
+#-hash
+#por los que me devuelve el output
+
+
+
+# y ejecutar deploy_after_manual_task.sh
+
+ 
+
--- ansible/deploy_after_manual_task.sh
+++ ansible/deploy_after_manual_task.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-ansible-playbook -i hosts -l master k8s-02-master_after_manual.yaml
-ansible-playbook -i hosts -l worker k8s-03-workers.yaml
+sudo ansible-playbook -i hosts -l master k8s-02-master_after_manual.yaml
+sudo ansible-playbook -i hosts -l master k8s-02-master_after_reboot.yaml
+sudo ansible-playbook -i hosts -l worker k8s-03-workers.yaml
+sudo ansible-playbook -i hosts -l worker,master k8s-02-worker_after_manual.yaml
+
+#[root@kubemaster ~]# kubectl get svc -A
+#De aquí cogeremos el puerto que se corresponde con haproxy-kubernetes-ingress (es el mapeao del 80). Por ejemplo:
+#haproxy-controller   haproxy-kubernetes-ingress                   NodePort    10.104.187.186   <none>        80:32489
+#Con este Puerto crearemos una nueva regla de seguridad con Terraform. En el archivo terraform/security_master.tf 
